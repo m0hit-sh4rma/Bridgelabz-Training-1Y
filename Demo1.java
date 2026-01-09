bc408952b0c18ffaa025020697224a0972035566
Mohit Sharma <mohitsharma41444@gmail.com>
2026-01-09 21:45:13 +0530

    Static, Non-static and Local Variable.[m1 & m2 method added.]

--- Demo1.java
+++ Demo1.java
@@ -2,11 +2,24 @@ public class Demo1{
 	static int x = 20;
 	int y = 30;
 	
+	public static void m2(){
+		int y = 50;
+		System.out.println("Local var of m2 'y': " + y);
+	}
+	
+	public static void m1(){
+		int y = 40;
+		System.out.println("Local var of m1 'y': " + y);
+		m2();
+	}
+	
 	public static void main(String[] args){
 		int z = 10;
 		
 		System.out.println("Local var z: " + z);
 		System.out.println("Static var x: " + x);
+		
+		m1();
 		
 		Demo1 obj = new Demo1();
 		
