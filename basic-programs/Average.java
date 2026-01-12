11343084a856ae7f4b52461ea5398c0a4624b2d0
Mohit Sharma <mohitsharma41444@gmail.com>
2026-01-12 13:00:25 +0530

    [Average.java file added]

---
+++ basic-programs/Average.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Average{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first number: ");
+		int a = sc.nextInt();
+		
+		System.out.print("Enter second number: ");
+		int b = sc.nextInt();
+		
+		System.out.print("Enter third number: ");
+		int c = sc.nextInt();
+		
+		double average = (a+b+c)/3;
+		
+		System.out.println("Average is: " + average);
+	}
+}
