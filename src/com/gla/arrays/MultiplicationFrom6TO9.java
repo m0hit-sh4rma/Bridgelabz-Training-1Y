ef802f20f10770e701a6d773506b5a5af9ab15a8
Mohit Sharma <mohitsharma41444@gmail.com>
2026-02-05 21:23:57 +0530

    [Mohit Sharma] updated MultiplicationFrom6To9.java

--- src/com/gla/arrays/MultiplicationFrom6TO9.java
+++ src/com/gla/arrays/MultiplicationFrom6TO9.java
@@ -12,7 +12,7 @@ public class MultiplicationFrom6TO9 {
 
         int[] arr = new int[4];
         int a = 0;
-        
+
         for (int i = 6; i <= 9; i++) {
             arr[a] = n * i;
             a++;
