e97034c6b576a46d311398bdc3ff3ddb38ed4035
Mohit Sharma <mohitsharma41444@gmail.com>
2026-02-05 21:08:26 +0530

    [Mohit Sharma] updated MultiplicationFrom6To9.java

---
+++ src/com/gla/arrays/MultiplicationFrom6TO9.java
@@ -0,0 +1,24 @@
+package com.gla.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiplicationFrom6TO9 {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        int[] arr = new int[4];
+        int a = 0;
+        
+        for (int i = 6; i <= 9; i++) {
+            arr[a] = n * i;
+            a++;
+            System.out.println(n + " * " + i + " = " + n*i);
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
